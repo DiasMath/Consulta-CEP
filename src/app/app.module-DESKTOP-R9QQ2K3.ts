6743a1559872c6c8824ade2c4bdd61c80eb86e1b
Matheus <matheusdiassandes@outlook.com>
2023-07-25 20:47:13 -0300

    commit de teste

---
+++ src/app/app.module-DESKTOP-R9QQ2K3.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+
+import { HeaderComponent } from './components/header/header.component';
+import { FormCepComponent } from './components/form-cep/form-cep.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FormCepComponent,
+    FooterComponent,
+    HomeComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/form-cep/form-cep.component-DESKTOP-R9QQ2K3.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-cep',
+  templateUrl: './form-cep.component.html',
+  styleUrls: ['./form-cep.component.css']
+})
+export class FormCepComponent {
+
+}
